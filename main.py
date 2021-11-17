cea94f8acf0dffc2d819d437a02fd261add7c12c
Snaiel <56014779+Snaiel@users.noreply.github.com>
2021-11-17 18:27:21 +0800

    added batch processing

--- main.py
+++ main.py
@@ -11,6 +11,26 @@ def print_media(media_files):
 
     print(tabulate(data, headers=['Index', 'File Name']))
 
+def create_process_list():
+    processes = []
+
+    while(True):
+        original_lesson = input('select the original lesson (enter Index): ')
+        new_audio_name = input('select the new audio: ')
+        final_lesson_name = input('set name of final file (default .mp4): ')
+
+        if len(final_lesson_name.split('.')) == 1:
+            final_lesson_name += '.mp4'
+
+        processes.append((original_lesson, new_audio_name, final_lesson_name))
+
+        if input('process another video? (y/n) ') == 'y':
+            continue
+        else:
+            break
+
+    return processes
+
 def main():
 
     media_path = getcwd() + '\\media'
@@ -18,20 +38,13 @@ def main():
 
     print_media(media_files)
 
-    lesson = input('select the original lesson (enter Index): ')
-    new_audio_name = input('select the new audio: ')
-    final_lesson_name = input('set name of final file (default .mp4): ')
+    processes = create_process_list()
 
-    if len(final_lesson_name.split('.')) == 1:
-        final_lesson_name += '.mp4'
-
-    print(final_lesson_name)
-
-    lesson = VideoFileClip('media/' + media_files[int(lesson)])
-
-    new_audio = AudioFileClip('media/' + media_files[int(new_audio_name)])
-    new_clip = lesson.set_audio(new_audio)
-    new_clip.write_videofile('media/' + final_lesson_name)
+    for process in processes:
+        lesson = VideoFileClip('media/' + media_files[int(process[0])])
+        new_audio = AudioFileClip('media/' + media_files[int(process[1])])
+        new_clip = lesson.set_audio(new_audio)
+        new_clip.write_videofile('media/' + process[2])
 
 if __name__ == "__main__":
     main()
